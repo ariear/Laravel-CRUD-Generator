77c8adc02bc39f6a9dc8cadbf5bc305ce36f6715
Arie <arieinjector@gmail.com>
2023-03-24 09:36:07 +0700

    add project to github

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily : {
+        'poppins' : ['"Poppins"', 'sans-serif']
+      }
+    },
+  },
+  corePlugins: {
+    // Remove the Tailwind CSS preflight styles so it can use Material UI's preflight instead (CssBaseline).
+    preflight: false,
+  },
+  plugins: [],
+}
