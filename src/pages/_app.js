733cc438f3e47e041572e7352f8e82a8c829dec2
Arie <arieinjector@gmail.com>
2023-03-25 09:55:45 +0700

    add result generate

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import '@/styles/globals.css'
+import "prismjs/themes/prism-tomorrow.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
--- src/pages/index.js
+++ src/pages/index.js
@@ -16,8 +16,10 @@ import { Input,
   Typography } from "@mui/material";
 import DeleteIcon from '@mui/icons-material/Delete';
 import Head from "next/head";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
+import Prism from 'prismjs'
+import "prismjs/components/prism-php";
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -32,7 +34,7 @@ function TabPanel(props) {
     >
       {value === index && (
         <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
+          <Typography component="span">{children}</Typography>
         </Box>
       )}
     </div>
@@ -47,7 +49,6 @@ function a11yProps(index) {
 }
 
 export default function Home() {
-  const [element, setElement] = useState('')
   const [columns, setColumns] = useState([
     {
       no : 1,
@@ -57,6 +58,7 @@ export default function Home() {
       validator: ''
     }
   ])
+  const [model, setModel] = useState('')
   const [isGenerate, setIsGenerate] = useState(false)
 
   const [valueTabs, setValueTabs] = useState(0)
@@ -81,6 +83,17 @@ export default function Home() {
     setIsGenerate(true)
   }
 
+  const handleInputChange = (e, index) => {
+    const { name, value } = e.target
+    const newColumn = [...columns]
+    newColumn[index][name] = value
+    setColumns(newColumn)
+  }
+
+  useEffect(() => {
+    Prism.highlightAll()
+  }, [])
+
   return (
     <>
       <Head>
@@ -93,7 +106,7 @@ export default function Home() {
         <form onSubmit={handleGenerate} className="bg-white p-4 shadow-lg rounded">
           <div className="mb-8">
             <p className="text-sm mb-1">Model Name</p>
-            <Input placeholder="ex: User" required />
+            <Input placeholder="ex: User" required value={model} onChange={(e) => setModel(e.target.value)} />
           </div>
 
           <TableContainer>
@@ -133,10 +146,10 @@ export default function Home() {
                         {index + 1}
                       </TableCell>
                       <TableCell align="left">
-                        <Input placeholder="input title" className="font-poppins text-sm" required />
+                        <Input placeholder="input title" className="font-poppins text-sm" name="title" value={column.title} onChange={(e) => handleInputChange(e, index)} required />
                       </TableCell>
                       <TableCell align="left">
-                        <Input placeholder="input data title" className="font-poppins text-sm" required />
+                        <Input placeholder="input data title" className="font-poppins text-sm" name="data_title" value={column.data_title} onChange={(e) => handleInputChange(e, index)} required />
                       </TableCell>
                       <TableCell align="left">
                       <FormControl className="w-[170px]">
@@ -145,19 +158,19 @@ export default function Home() {
                           required
                           labelId="demo-simple-select-label"
                           id="demo-simple-select"
-                          value={element}
+                          value={column.element}
                           label="Element"
-                          onChange={(e) => setElement(e.target.value)}
+                          name="element" onChange={(e) => handleInputChange(e, index)}
                         >
-                          <MenuItem value={10}>Input Text</MenuItem>
-                          <MenuItem value={20}>Input Number</MenuItem>
-                          <MenuItem value={30}>Select</MenuItem>
-                          <MenuItem value={30}>Textarea</MenuItem>
+                          <MenuItem value={'string'}>Input Text</MenuItem>
+                          <MenuItem value={'integer'}>Input Number</MenuItem>
+                          <MenuItem value={'enum'}>Select</MenuItem>
+                          <MenuItem value={'text'}>Textarea</MenuItem>
                         </Select>
                       </FormControl>
                       </TableCell>
                       <TableCell align="left">
-                        <Input placeholder="input validator" className="font-poppins text-sm" required />
+                        <Input placeholder="input validator" className="font-poppins text-sm" name="validator" value={column.validator} onChange={(e) => handleInputChange(e, index)} required />
                       </TableCell>
                       <TableCell align="left">
                         <DeleteIcon color="error" onClick={() => handleDeleteColumn(column.no)} />
@@ -174,7 +187,7 @@ export default function Home() {
         </div>
         {
           isGenerate &&
-            <>
+          <>
               <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
                 <Tabs value={valueTabs} onChange={(event, newValueTabs) => setValueTabs(newValueTabs)} aria-label="basic tabs example">
                   <Tab className="capitalize font-poppins" label="Basic" {...a11yProps(0)} /> 
@@ -182,10 +195,287 @@ export default function Home() {
                 </Tabs>
               </Box>
               <TabPanel value={valueTabs} index={0}>
-                Item One
+                  <p className="font-poppins text-sm font-medium mb-2">Command</p>
+                  <pre className="bg-[#343541] p-4 rounded-md mb-6"><code className="language-prism-php">php artisan make:model {model} -mcr</code></pre>
+
+                  <p className="font-poppins text-sm font-medium mb-2">Route</p>
+                  <pre className="bg-[#343541] p-4 rounded-md mb-6">
+                    <code className="language-prism-php">
+                      {`use App\\Http\\Controllers\\${model}Controller; 
+
+Route::resource('/${model.toLowerCase()}', ${model}Controller::class);`}
+                    </code>
+                  </pre>
+
+                  <p className="font-poppins text-sm font-medium mb-2">{model}Controller</p>
+                  <pre className="bg-[#343541] p-4 rounded-md mb-6">
+                    <code className="language-prism-php">
+                      
+                    </code>
+                  </pre>
+
+                  <p className="font-poppins text-sm font-medium mb-2">Model</p>
+                  <pre className="bg-[#343541] p-4 rounded-md mb-6">
+                    <code className="language-prism-php">
+                    {`<?php
+
+namespace App\\Models;
+
+use Illuminate\\Database\\Eloquent\\Factories\\HasFactory;
+use Illuminate\\Database\\Eloquent\\Model;
+
+class ${model} extends Model
+{
+    use HasFactory;
+    protected $guarded = [];
+    protected $table = '${model.toLowerCase()}';
+}
+`}
+                    </code>
+                  </pre>
+
+                  <p className="font-poppins text-sm font-medium mb-2">Migration</p>
+                  <pre className="bg-[#343541] p-4 rounded-md mb-6">
+                    <code className="language-prism-php">
+                    {`<?php
+
+use Illuminate\\Database\\Migrations\\Migration;
+use Illuminate\\Database\\Schema\\Blueprint;
+use Illuminate\\Support\\Facades\\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('${model.toLowerCase()}', function (Blueprint $table) {
+            $table->id();
+            ${columns.map(column => `$table->${column.element}('${column.data_title}'${column.element === 'enum' ? ',[opt1, opt2]' : ''}); \n`).join('')}
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('${model.toLowerCase()}');
+    }
+};
+`}
+                    </code>
+                  </pre>
+
+                  <p className="font-poppins text-sm font-medium mb-2">Index Table</p>
+                  <pre className="bg-[#343541] p-4 rounded-md mb-6">
+                    <code className="language-prism-php">
+                      
+                    </code>
+                  </pre>
+
+                  <p className="font-poppins text-sm font-medium mb-2">Create Form</p>
+                  <pre className="bg-[#343541] p-4 rounded-md mb-6">
+                    <code className="language-prism-php">
+                      
+                    </code>
+                  </pre>
+
+                  <p className="font-poppins text-sm font-medium mb-2">Edit Form</p>
+                  <pre className="bg-[#343541] p-4 rounded-md mb-6">
+                    <code className="language-prism-php">
+                      
+                    </code>
+                  </pre>
               </TabPanel>
               <TabPanel value={valueTabs} index={1}>
-                Item Two
+                  <p className="font-poppins text-sm font-medium mb-2">Command</p>
+                  <pre className="bg-[#343541] p-4 rounded-md mb-6"><code className="language-prism-php">php artisan make:model {model} -mcr</code></pre>
+
+                  <p className="font-poppins text-sm font-medium mb-2">Route *api.php</p>
+                  <pre className="bg-[#343541] p-4 rounded-md mb-6">
+                    <code className="language-prism-php">
+                      {`use App\\Http\\Controllers\\${model}Controller; 
+
+Route::resource('/${model.toLowerCase()}', ${model}Controller::class);`}
+                    </code>
+                  </pre>
+
+                  <p className="font-poppins text-sm font-medium mb-2">{model}Controller</p>
+                  <pre className="bg-[#343541] p-4 rounded-md mb-6">
+                    <code className="language-prism-php">
+                      {`<?php
+
+namespace App\\Http\\Controllers;
+
+use App\\Models\\${model};
+use Illuminate\\Http\\Request;
+use Illuminate\\Support\\Facades\\Validator;
+
+class ${model}Controller extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json([
+            'data' => ${model}::all()
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validate = Validator::make($request->all(), [
+            ${columns.map(column => `'${column.data_title}' => '${column.validator}', \n`).join('')}
+        ]);
+
+        if ($validate->fails()) {
+            return response()->json($validate->errors(), 422);
+        }
+
+        $${model.toLowerCase()} = ${model}::create([
+            ${columns.map(column => `'${column.data_title}' => $request->${column.data_title}, \n`).join('')}
+        ]);
+
+        if ($${model.toLowerCase()}) {
+            return response()->json([
+                'message' => 'Success Add ${model}',
+                'data' => $${model.toLowerCase()}
+            ], 201);
+        }
+
+        return response()->json([
+            'message' => 'Fail Add ${model}'
+        ], 409);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        $${model.toLowerCase()} = ${model}::find($id);
+
+        if (!$${model.toLowerCase()}) {
+            return response()->json([
+                'message' => '${model} not found'
+            ], 404);
+        }
+
+        return response()->json([
+            '${model.toLowerCase()}' => $${model.toLowerCase()}
+        ], 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, $id)
+    {
+        $${model.toLowerCase()} = ${model}::find($id);
+
+        if (!$${model.toLowerCase()}) {
+            return response()->json([
+                'message' => '${model} not found'
+            ], 404);
+        }
+
+        $validate = Validator::make($request->all(), [
+            ${columns.map(column => `'${column.data_title}' => '${column.validator}', \n`).join('')}
+        ]);
+
+        if ($validate->fails()) {
+            return response()->json($validate->errors(), 422);
+        }
+
+        $data = $${model.toLowerCase()}->update([
+            ${columns.map(column => `'${column.data_title}' => $request->${column.data_title}, \n`).join('')}
+        ]);
+
+        if ($data) {
+            return response()->json([
+                'message' => 'Success Update ${model}',
+                'data' => $data
+            ], 200);
+        }
+
+        return response()->json([
+            'message' => 'Fail Update ${model}'
+        ], 409);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        $${model.toLowerCase()} = ${model}::find($id);
+
+        if (!$${model.toLowerCase()}) {
+            return response()->json([
+                'message' => '${model} not found'
+            ], 404);
+        }
+
+        $${model.toLowerCase()}->delete();
+
+        response()->json([
+            'message' => 'Success Delete ${model}'
+        ], 200);
+    }
+}
+`}
+                    </code>
+                  </pre>
+
+                  <p className="font-poppins text-sm font-medium mb-2">Model</p>
+                  <pre className="bg-[#343541] p-4 rounded-md mb-6">
+                    <code className="language-prism-php">
+                      {`<?php
+
+namespace App\\Models;
+
+use Illuminate\\Database\\Eloquent\\Factories\\HasFactory;
+use Illuminate\\Database\\Eloquent\\Model;
+
+class ${model} extends Model
+{
+    use HasFactory;
+    protected $guarded = [];
+    protected $table = '${model.toLowerCase()}';
+}
+`}
+                    </code>
+                  </pre>
+
+                  <p className="font-poppins text-sm font-medium mb-2">Migration</p>
+                  <pre className="bg-[#343541] p-4 rounded-md mb-6">
+                    <code className="language-prism-php">
+                      {`<?php
+
+use Illuminate\\Database\\Migrations\\Migration;
+use Illuminate\\Database\\Schema\\Blueprint;
+use Illuminate\\Support\\Facades\\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('${model.toLowerCase()}', function (Blueprint $table) {
+            $table->id();
+            ${columns.map(column => `$table->${column.element}('${column.data_title}'${column.element === 'enum' ? ',[opt1, opt2]' : ''}); \n`).join('')}
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('${model.toLowerCase()}');
+    }
+};
+`}
+                    </code>
+                  </pre>
               </TabPanel>
             </>
         }
